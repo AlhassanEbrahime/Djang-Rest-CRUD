a8637e030c19c7db683fb5aba90ee001709403e0
Al-hasan ebraheem <sonanomail@gmail.com>
2024-10-26 22:54:44 +0300

    adding frontend template

--- task_drf/api/views.py
+++ task_drf/api/views.py
@@ -47,9 +47,11 @@ def task_update(request,pk):
     serializer = TaskSerializer(instance=task , data = request.data)
     
     if serializer.is_valid():
-        serializer.save()
-    
-    return Response(serializer.data) 
+            serializer.save()
+            return Response(serializer.data, status=201)
+        
+    return Response(serializer.errors, status=400)
+
 
 
 @api_view(['DELETE'])
